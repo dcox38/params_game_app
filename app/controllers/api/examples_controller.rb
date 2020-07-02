6e33b44d440660379137ca64521ea2a253a7ca7a
Dan <dcox38@gmailL.com>
2020-07-01 20:34:53 -0500

    add url segment paramters

--- app/controllers/api/examples_controller.rb
+++ app/controllers/api/examples_controller.rb
@@ -1,2 +1,24 @@
 class Api::ExamplesController < ApplicationController
+
+  def name
+    p "params"
+    @name = params['name']
+    render 'name.json.jb'
+  end
+
+  def guess
+    
+    correct_answer = 42
+    user_guess = params['guess'].to_i
+
+    if user_guess > correct_answer
+      @message = "You guessed too high"
+    elsif user_guess < correct_answer
+      @message = "You guessed too low"
+    else
+      @message = "You nailed it!"
+    end
+    render 'number_guess.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/your_name" => "examples#name"
+
+    get "/url_segment_param/:guess" => "examples#guess"
+
+
+  end
 end
